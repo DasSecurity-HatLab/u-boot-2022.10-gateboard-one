97b195ba11b85a96b22b0ebe448aa509b2518c5e
Aodzip <aodzip@gmail.com>
2023-02-23 11:23:31 +0800

    增加三种时钟速率

--- helper/build.sh
+++ helper/build.sh
@@ -6,7 +6,16 @@ JOBS=`grep -c ^processor /proc/cpuinfo`
 mkdir -p ./build-output
 
 export CROSS_COMPILE=mipsel-linux-gnu-
-make O=build-hatlab_gateboard-one hatlab_gateboard_one_defconfig
 cp ./mt7621_stage_sram.blob ./build-hatlab_gateboard-one/mt7621_stage_sram.bin
+
+make O=build-hatlab_gateboard-one hatlab_gateboard_one_880_defconfig
 make O=build-hatlab_gateboard-one -j${JOBS}
-cp build-hatlab_gateboard-one/u-boot-mt7621.bin ./build-output/hatlab_gateboard-one-bios.bin
+cp build-hatlab_gateboard-one/u-boot-mt7621.bin ./build-output/hatlab_gateboard-one-bios.880MHz.bin
+
+make O=build-hatlab_gateboard-one hatlab_gateboard_one_1000_defconfig
+make O=build-hatlab_gateboard-one -j${JOBS}
+cp build-hatlab_gateboard-one/u-boot-mt7621.bin ./build-output/hatlab_gateboard-one-bios.1000MHz.bin
+
+make O=build-hatlab_gateboard-one hatlab_gateboard_one_1200_defconfig
+make O=build-hatlab_gateboard-one -j${JOBS}
+cp build-hatlab_gateboard-one/u-boot-mt7621.bin ./build-output/hatlab_gateboard-one-bios.1200MHz.bin
